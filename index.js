1695b9544cc4b5407ebd53764127e85c657bd1fa
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 00:52:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ bot.on("message", async message => {
 
   if (cmd === `${prefix}numero`){
     var estratto = Math.floor(Math.random() * 90) + 1;
+    switch(n) {
+      case 1,2,3,4,5,6,7,8,9:
+        estratto = "0" + estratto + "! http://euea.altervista.org/n/0" + estratto + ".jpg";
+        break;
+      case 10:
+        estratto = "0" + estratto + "! http://euea.altervista.org/n/10.jpg";
+        break;
+      default:
+        cestratto = estratto + "! 'http://euea.altervista.org/n/" + estratto + ".jpg";
+    }
     message.channel.send("Pong2!" + estratto);
   }
 });
