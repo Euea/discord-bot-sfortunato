db7adc4579553a6e5c303b47214c1af0da2d3952
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 00:35:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+  console.log(`${bot.user.username} online!`);
+  bot.user.setActivity(`niende`);
 });
 
 bot.on("message", async message => {
@@ -11,13 +11,13 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '-';
+  let prefix = '.';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
   if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+    message.channel.send("Pong2!");
   }
 });
 
