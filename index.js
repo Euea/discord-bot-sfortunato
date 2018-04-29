73427a3f3c5569f77eef734ee2dbf7700a8982dc
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 18:55:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 const Discord = require("discord.js")
 const fs = require("fs");
-const mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "process.env.dbhost",
-  user: "process.env.dbuser",
-  password: "process.env.dbpass"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-
-
 const dbfile = "./database.sqlite";
 const db = require("sqlite");
 db.open(dbfile);
