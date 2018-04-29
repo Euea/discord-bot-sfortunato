a8f85e7cb47012747de1ae3b787bedb90c13e094
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 17:57:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,18 +49,10 @@ bot.on("message", async message => {
     }
     
     // MONETA - Testa o croce
-    else if((chan == `games` || chan == `spam-musica`) && text == `.moneta2`) {
-      bot.user.setActivity(`testa o croce`);
-      var estratto = Math.floor(Math.random() * 2) + 1;
-      message.channel.send(`${estratto}`);
-      return;
-    }
-    
-    // MONETA - Testa o croce
     else if((chan == `games` || chan == `spam-musica`) && text == `.moneta`) {
       bot.user.setActivity(`testa o croce`);
-      var estratto = Math.floor(Math.random() * 2) + 1;
-      if(estratto < 2) {
+      var estratto = Math.floor(Math.random() * 2);
+      if(estratto < 1) {
         estratto = `croce`;
       }
       else {
@@ -75,45 +67,51 @@ bot.on("message", async message => {
       bot.user.setActivity(`tombola`);
       db.get(`SELECT * FROM tombola WHERE managerid ="${message.author.toString()}" LIMIT 1`).then(row => {
         if(!row) {
-          db.run(`INSERT INTO tombola (managerid, numbers) VALUES (?, ?)`, [message.author.toString(), "0"]);
+          var resetnumb = `01`;
+          var i;
+          for (i = 2; i < 91; i++) { 
+            if(i < 10) {
+              numero = `0${i}`;
+            }
+            else {
+              numero = `${i}`;
+            }
+            resetnumb = `${resetnumb}, ${numero}`);
+          }
+          db.run(`INSERT INTO tombola (managerid, numbers) VALUES (?, ?)`, [message.author.toString(), resetnumb]);
           message.channel.send(`${message.author.toString()} ha avviato una nuova partita di tombola.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
         }
         else {
-          message.channel.send(`${message.author.toString()} hai già avviato una partita di tombola.\nNumeri estratti: ${row.numbers}.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
+          message.channel.send(`${message.author.toString()} hai già avviato una partita di tombola.\nNumeri estratti: ${row.mancanti}.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
         }
         return;
       });
     }
     
     else if((chan == `games` || chan == `spam-musica`) && text == `.tombola numero`) {
-      var estratto = Math.floor(Math.random() * 90) + 1;
-      var numero = ``;
-      var upnumeri = ``;
-      if(estratto < 10) {
-        numero = `0${estratto}`;
-      }
-      else {
-        numero = `${estratto}`;
-      }
-      message.channel.send(`${message.author.toString()} ha estratto il numero: http://euea.altervista.org/numero/v1/${numero}.png`);
-      db.get(`SELECT * FROM tombola WHERE managerid ="${message.author.toString()}" LIMIT 1`).then(row => {
+      db.get(`SELECT * FROM tombola WHERE gestore ="${message.author.toString()}" LIMIT 1`).then(row => {
         if(!row) {
           message.channel.send(`Errore, per favore contatta l'amministratore del bot.`);
         }
         else {
-          if(`${row.numbers}` == "0") {
-            upnumeri = `${numero}`;
-          }
-          else {
-            upnumeri = `${row.numbers}, ${numero}`;
-          }
-          db.run(`UPDATE tombola SET numbers = "${upnumeri}" WHERE managerid = "${message.author.toString()}"`);
-          message.channel.send(`${message.author.toString()} i tuoi numeri estratti: ${upnumeri}.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
+          var dbestratti = ${row.estratti};
+          var dbmancanti = ${row.mancanti};
+          
+          var arraymancanti = dbmancanti.split(", ");
+          var estratto = arraymancanti[Math.floor(Math.random() * arraymancanti.length)];
+          
+          var upestratti = `${dbestratti}, ${estratto}`;
+          var upmancanti = dbmancanti.replace(`, ${estratto}`, ``);
+          
+          db.run(`UPDATE tombola SET mancanti = "${upmancanti}", estratti = "${upestratti}" WHERE gestore = "${message.author.toString()}"`);
+          
+          message.channel.send(`${message.author.toString()} ha estratto il numero: http://euea.altervista.org/lotto/v1/${numero}.png\nTutti i numeri estratti: ${upestratti}.\nScrivi ".tombola numero" per estrarre un altro numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
         }
-      return;
+        return;
       });
     }
     
+    // Continuo qui
     else if((chan == `games` || chan == `spam-musica`) && text == `.tombola fine`) {
       message.channel.send(`${message.author.toString()} ha terminato la partita di tombola. Numeri estratti: . Scrivi ".tombola" per iniziare una nuova partita.`);
       return;
@@ -121,7 +119,7 @@ bot.on("message", async message => {
     
     else if(authorid == admin && (chan == `games` || chan == `spam-musica`) && text == `.resetdb`) {
       db.run("DROP TABLE IF EXISTS tombola");
-      db.run(`CREATE TABLE IF NOT EXISTS tombola (managerid TEXT, numbers TEXT)`);
+      db.run(`CREATE TABLE IF NOT EXISTS tombola (gestore TEXT, mancanti TEXT, estratti TEXT)`);
       message.channel.send(`Database resettato.`);
       return;
     }
