a9d94066fe08b75b30594b28e20de5ece637bf29
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 15:48:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ db.open(dbfile);
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-  db.run(`CREATE TABLE IF NOT EXISTS tombola (gameid TEXT, managerid TEXT, numbers TEXT)`);
+  db.run(`CREATE TABLE IF NOT EXISTS tombola (managerid TEXT, numbers TEXT)`);
   bot.user.setActivity(`niende`);
   console.log(`Bot online!`);
 });
@@ -26,16 +26,31 @@ bot.on("message", async message => {
       bot.user.setActivity(`dare i numeri`);
       var estratto = Math.floor(Math.random() * 90) + 1;
       if(estratto < 10) {
-          message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/0${estratto}.png`);
+        message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/0${estratto}.png`);
       }
       else {
-          message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/${estratto}.png`);
+        message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/${estratto}.png`);
       }
     }
     
     else if(chan == `games` && text == `.tombola`) {
-          bot.user.setActivity(`tombola`);
-          message.channel.send(`Tombola`);
+      bot.user.setActivity(`tombola`);
+      message.channel.send(`Tombola`);
+
+      db.get(`SELECT * FROM tombola WHERE managerid ="${message.author.toString()}" LIMIT 1`).then(row => {
+        if(!row) {
+          db.run(`INSERT INTO tombola (managerid, numbers) VALUES (?, ?)`, [message.author.toString(), ""]);
+          message.channel.send(`Non esiste, lo creo.`);
+        }
+        else {
+          message.channel.send(`Esiste già.`);
+        }
+      });
+      
+      else if(chan == `games` && text == `.resetdb`) {
+        db.run("DROP TABLE IF EXISTS users");
+        message.channel.send(`Database resettato.`);
+      }
     }
   }
 });
