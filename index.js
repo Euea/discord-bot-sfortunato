6aec3f689f6574d2adfb675031a7efa98be2d780
Euea <38819420+Euea@users.noreply.github.com>
2018-04-29 13:42:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
+const fs = require("fs");
+const dbfile = "./database.sqlite";
+const db = require("sqlite");
+db.open(dbfile);
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
@@ -7,21 +11,28 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
-  let prefix = '.';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  if(message.author.bot) {
+    return;
+  }
+  else if((message.channel.type === "dm") {
+    return;
+  }
+  else {
+    let prefix = '.';
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}numero`){
-    let smorfia = ["Saab", "Volvo", "BMW"];
-    let estratto = Math.floor(Math.random() * 90) + 1;
-    if(estratto < 11) {
-        message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/0${estratto}.png`);
-    }
-    else {
-        message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/${estratto}.png`);
+  
+    if(message.channel.name == "games" && cmd === `${prefix}numero`) {
+      let smorfia = ["Saab", "Volvo", "BMW"];
+      let estratto = Math.floor(Math.random() * 90) + 1;
+      if(estratto < 10) {
+          message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/0${estratto}.png`);
+      }
+      else {
+          message.channel.send(`${message.author.toString()} estrae: http://euea.altervista.org/numero/v1/${estratto}.png`);
+      }
     }
   }
 });
