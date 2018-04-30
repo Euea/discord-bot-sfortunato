df911e88d1350f6c93186ae57caed7a3b2af2cff
Euea <38819420+Euea@users.noreply.github.com>
2018-04-30 23:42:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const admin = process.env.admin;
 
 bot.on("ready", async () => {
   db.run("DROP TABLE IF EXISTS tombola");
-  db.run(`CREATE TABLE IF NOT EXISTS tombola (gestore TEXT, mancanti TEXT, estratti TEXT)`);
-  bot.user.setActivity(`niende`);
-  console.log(`Bot online!`);
+  db.run('CREATE TABLE IF NOT EXISTS tombola (gestore TEXT, mancanti TEXT, estratti TEXT)');
+  //bot.user.setActivity('niende');
+  console.log('Bot online!');
 });
 
 bot.on("message", async message => {
@@ -28,78 +28,119 @@ bot.on("message", async message => {
     var rcolor = colors[Math.floor(Math.random() * 18)];
     
     // MONETA - Testa o croce
-    if((chan == `games` || chan == `spam-musica`) && text == `.moneta`) {
-      bot.user.setActivity(`testa o croce`);
+    if((chan == 'games' || chan == 'spam-musica') && text == '.moneta') {
+      //bot.user.setActivity('testa o croce');
       var estratto = Math.floor(Math.random() * 2);
       if(estratto < 1) {
-        estratto = `croce`;
+        estratto = 'croce';
       }
       else {
-        estratto = `testa`;
+        estratto = 'testa';
       }
-      message.channel.send(`${message.author.toString()} lancia una moneta: http://euea.altervista.org/moneta/v1/${estratto}.png`);
+      postembed = new Discord.RichEmbed()
+        .setColor('#'+rcolor)
+        .setAuthor('»', message.author.avatarURL, '')
+        .setDescription(message.author.toString() + ' ha lanciato una moneta...\nEd esce '+estratto+'.')
+        .setThumbnail('https://euea.altervista.org/moneta/v1/'+estratto+'.png')
+        .setFooter('TESTA O CROCE', '')
+        .setTimestamp();
+      message.channel.send(postembed);
       message.delete(message);
       return;
     }
     
     // DADO - Numero casuale da 1 a 6
-    else if((chan == `games` || chan == `spam-musica`) && text == `.dado`) {
-      bot.user.setActivity(`dadi`);
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.dado') {
+      //bot.user.setActivity('dadi');
       var estratto = Math.floor(Math.random() * 6) + 1;
-      message.channel.send(`${message.author.toString()} tira un dado: http://euea.altervista.org/dado/v1/${estratto}.png`);
+      postembed = new Discord.RichEmbed()
+        .setColor('#'+rcolor)
+        .setAuthor('»', message.author.avatarURL, '')
+        .setDescription(message.author.toString() + ' ha tirato un dado...\nEd esce '+estratto+'.')
+        .setThumbnail('https://euea.altervista.org/dado/v1/'+estratto+'.png')
+        .setFooter('TIRA IL DADO', '')
+        .setTimestamp();
+      message.channel.send(postembed);
       message.delete(message);
       return;
     }
     
     // ROULETTE - Numero casuale da 0 a 36
-    else if((chan == `games` || chan == `spam-musica`) && text == `.roulette`) {
-      bot.user.setActivity(`roulette`);
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.roulette') {
+      //bot.user.setActivity('roulette');
       var estratto = Math.floor(Math.random() * 37);
-      message.channel.send(`${message.author.toString()} fa un tiro alla roulette: http://euea.altervista.org/roulette/v1/${estratto}.png`);
+      postembed = new Discord.RichEmbed()
+        .setColor('#'+rcolor)
+        .setAuthor('»', message.author.avatarURL, '')
+        .setDescription(message.author.toString() + ' fa girare la roulette...\nEd esce '+estratto+'.')
+        .setThumbnail('https://euea.altervista.org/roulette/v1/'+estratto+'.png')
+        .setFooter('ROULETTE', '')
+        .setTimestamp();
+      message.channel.send(postembed);
       message.delete(message);
       return;
     }
     
     // LOTTO - Numero casuale da 1 a 90
-    else if((chan == `games` || chan == `spam-musica`) && text == `.lotto`) {
-      bot.user.setActivity(`lotto`);
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.lotto') {
+      //bot.user.setActivity('lotto');
       var estratto = Math.floor(Math.random() * 90) + 1;
-      console.log(`${estratto}`);
-      var numero = ``;
+      var numero = '';
       if(estratto < 10) {
-        numero = `0${estratto}`;
+        numero = '0'+estratto;
       }
       else {
-        numero = `${estratto}`;
+        numero = estratto;
       }
-      message.channel.send(`${message.author.toString()} ha estratto il numero: http://euea.altervista.org/lotto/v1/${numero}.png`);
+      postembed = new Discord.RichEmbed()
+        .setColor('#'+rcolor)
+        .setAuthor('»', message.author.avatarURL, '')
+        .setDescription(message.author.toString() + ' estrae un numero...\nEd esce '+estratto+'.')
+        .setThumbnail('https://euea.altervista.org/lotto/v1/'+estratto+'.png')
+        .setFooter('LOTTO', '')
+        .setTimestamp();
+      message.channel.send(postembed);
       message.delete(message);
       return;
     }
     
     // TOMBOLA
-    else if((chan == `games` || chan == `spam-musica`) && text == `.tombola`) {
-      bot.user.setActivity(`tombola`);
-      db.run(`CREATE TABLE IF NOT EXISTS tombola (gestore TEXT, mancanti TEXT, estratti TEXT)`);
-      db.get(`SELECT * FROM tombola WHERE gestore ="${message.author.toString()}" LIMIT 1`).then(row => {
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.tombola') {
+      //bot.user.setActivity('tombola');
+      db.run('CREATE TABLE IF NOT EXISTS tombola (gestore TEXT, mancanti TEXT, estratti TEXT)');
+      db.get('SELECT * FROM tombola WHERE gestore ="'+message.author.toString()+'" LIMIT 1').then(row => {
         if(!row) {
-          var mancanti = `01`;
+          var mancanti = '01';
           var i;
           for (i = 2; i < 91; i++) { 
             if(i < 10) {
-              numero = `0${i}`;
+              numero = '0'+i;
             }
             else {
-              numero = `${i}`;
+              numero = i;
             }
-            mancanti = `${mancanti}, ${numero}`;
+            mancanti = mancanti+', '+numero;
           }
-          var estratti = `0`;
-          db.run(`INSERT INTO tombola (gestore, estratti, mancanti) VALUES (?, ?, ?)`, [message.author.toString(), `${estratti}`, `${mancanti}`]);
-          message.channel.send(`${message.author.toString()} ha avviato una nuova partita di tombola.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
+          var estratti = '0';
+          db.run('INSERT INTO tombola (gestore, estratti, mancanti) VALUES (?, ?, ?)', [message.author.toString(), estratti, mancanti]);
+          postembed = new Discord.RichEmbed()
+            .setColor('#'+rcolor)
+            .setAuthor('»', message.author.avatarURL, '')
+            .setDescription(message.author.toString() + ' ha avviato una nuova partita...\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".')
+            .setThumbnail('https://euea.altervista.org/tombola/v1/00.png')
+            .setFooter('TOMBOLA', '')
+            .setTimestamp();
+          message.channel.send(postembed);
         }
         else {
-          message.channel.send(`${message.author.toString()} hai già avviato una partita di tombola.\nNumeri estratti: ${row.estratti}.\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
+          postembed = new Discord.RichEmbed()
+            .setColor('#'+rcolor)
+            .setAuthor('»', message.author.avatarURL, '')
+            .setDescription(message.author.toString() + ' hai già avviato una partita...\nScrivi ".tombola numero" per estrarre un numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".')
+            .setThumbnail('https://euea.altervista.org/tombola/v1/00.png')
+            .setFooter('TOMBOLA', '')
+            .setTimestamp();
+          message.channel.send(postembed);
         }
         message.delete(message);
         return;
@@ -107,34 +148,55 @@ bot.on("message", async message => {
     }
     
     // TOMBOLA NUMERO
-    else if((chan == `games` || chan == `spam-musica`) && text == `.tombola numero`) {
-      db.get(`SELECT * FROM tombola WHERE gestore ="${message.author.toString()}" LIMIT 1`).then(row => {
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.tombola numero') {
+      db.get('SELECT * FROM tombola WHERE gestore ="'+message.author.toString()+'" LIMIT 1').then(row => {
         if(!row) {
-          message.channel.send(`${message.author.toString()} non hai attivato alcuna partita. Scrivi ".tombola" per iniziare una nuova partita.`);
+          postembed = new Discord.RichEmbed()
+            .setColor('#'+rcolor)
+            .setAuthor('»', message.author.avatarURL, '')
+            .setDescription(message.author.toString() + ' non hai avviato alcuna partita...\nScrivi ".tombola" per avviare una nuova partita.')
+            .setThumbnail('https://euea.altervista.org/tombola/v1/00.png')
+            .setFooter('TOMBOLA', '')
+            .setTimestamp();
+          message.channel.send(postembed);
         }
         else {
-          var dbestratti = `${row.estratti}`;
-          var dbmancanti = `${row.mancanti}`;
+          var dbestratti = row.estratti;
+          var dbmancanti = row.mancanti;
           
           var arraymancanti = dbmancanti.split(", ");
           var estratto = arraymancanti[Math.floor(Math.random() * arraymancanti.length)];
           
-          if(dbestratti === `0`){
-            var upestratti = `${estratto}`;
+          if(dbestratti === '0'){
+            var upestratti = estratto;
           }
           else {
-            var upestratti = `${dbestratti}, ${estratto}`;
+            var upestratti = dbestratti+', 'estratto;
           }
-          var upmancanti = dbmancanti.replace(`, ${estratto}`, ``);
-          var upmancanti = upmancanti.replace(`${estratto}, `, ``);
+          var upmancanti = dbmancanti.replace(', '+estratto, '');
+          var upmancanti = upmancanti.replace(estratto+', ', '');
           
           if(arraymancanti.length < 2) {
-            message.channel.send(`${message.author.toString()} ha estratto il numero: http://euea.altervista.org/lotto/v1/${estratto}.png\nTutti i numeri sono stati estratti, la partita è conclusa.\nScrivi ".tombola" per iniziare una nuova partita.`);
-            db.run(`DELETE FROM tombola WHERE gestore = "${message.author.toString()}"`);
+            db.run('DELETE FROM tombola WHERE gestore = "'+message.author.toString()+'"');
+            postembed = new Discord.RichEmbed()
+              .setColor('#'+rcolor)
+              .setAuthor('»', message.author.avatarURL, '')
+              .setDescription(message.author.toString() + ' estrae un numero...\nEd esce '+estratto+'. Tutti i numeri sono stati estratti, la partita è conclusa.')
+              .setThumbnail('https://euea.altervista.org/tombola/v1/'+estratto+'.png')
+              .setFooter('TOMBOLA', '')
+              .setTimestamp();
+            message.channel.send(postembed);
           }
           else {
-            db.run(`UPDATE tombola SET mancanti = "${upmancanti}", estratti = "${upestratti}" WHERE gestore = "${message.author.toString()}"`);
-            message.channel.send(`${message.author.toString()} ha estratto il numero: http://euea.altervista.org/lotto/v1/${estratto}.png\nTutti i numeri estratti: ${upestratti}.\nScrivi ".tombola numero" per estrarre un altro numero, scrivi ".tombola fine" per chiudere la partita e poterne iniziare una nuova con il comando ".tombola".`);
+            db.run('UPDATE tombola SET mancanti = "'+upmancanti+'", estratti = "'+upestratti+'" WHERE gestore = "'+message.author.toString()+'"');
+            postembed = new Discord.RichEmbed()
+              .setColor('#'+rcolor)
+              .setAuthor('»', message.author.avatarURL, '')
+              .setDescription(message.author.toString() + ' estrae un numero...\nEd esce '+estratto+'. Tutti i numeri estratti: '+upestratti+'.')
+              .setThumbnail('https://euea.altervista.org/tombola/v1/'+estratto+'.png')
+              .setFooter('TOMBOLA', '')
+              .setTimestamp();
+            message.channel.send(postembed);
           }
         }
         message.delete(message);
@@ -143,23 +205,23 @@ bot.on("message", async message => {
     }
     
     // TOMBOLA FINE
-    else if((chan == `games` || chan == `spam-musica`) && text == `.tombola fine`) {
-      message.channel.send(`${message.author.toString()} ha terminato la partita di tombola. Scrivi ".tombola" per iniziare una nuova partita.`);
-      db.run(`DELETE FROM tombola WHERE gestore = "${message.author.toString()}"`);
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.tombola fine') {
+      message.channel.send(message.author.toString()+' ha interrotto la partita di tombola.\nScrivi ".tombola" per iniziare una nuova partita.');
+      db.run('DELETE FROM tombola WHERE gestore = "'+message.author.toString()+'"');
       message.delete(message);
       return;
     }
     
     // TEST
-    else if((chan == `games` || chan == `spam-musica`) && text == `.euea`) {
-      exampleEmbed = new Discord.RichEmbed()
+    else if((chan == 'games' || chan == 'spam-musica') && text == '.euea') {
+      postembed = new Discord.RichEmbed()
         .setColor('#'+rcolor)
         .setAuthor('»', message.author.avatarURL, '')
         .setDescription(message.author.toString() + ' ha avviato una nuova partita.\nScrivi .tombola aiuto per info sui comandi disponibili.')
         .setThumbnail('https://euea.altervista.org/lotto/v1/01.png')
         .setFooter('TOMBOLA', '')
         .setTimestamp();
-      message.channel.send(exampleEmbed);
+      message.channel.send(postembed);
       return;
     }
 
